5b914ed2db43a36e5bc355e371cf5bcb700765d4
ZixuanLi-China <103539035+ZixuanLi-China@users.noreply.github.com>
2022-04-15 00:04:34 +0800

    Add files via upload

---
+++ MutilTucker/kernel.h
@@ -0,0 +1,28 @@
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+#include <cuda_fp16.h>
+
+#define type_of_data float
+
+void Mutil_Update_Parameter_A(const int order, const int gpu_number,
+		const int part_per_gpu, const int core_length, const int core_dimen,
+		const int *multi_nnz_train, type_of_data **multi_value_train_device,
+		int **multi_index_train_device, type_of_data*** parameter_a_device,
+		type_of_data*** parameter_a_host_to_device, type_of_data*** g_device,
+		int** multi_start, int** multi_part, type_of_data learn_rate_a,
+		type_of_data lambda_a, double *memcpy_time_a, const int model);
+
+void Mutil_Update_Parameter_G(const int order, const int gpu_number,
+		const int nnz, const int part_per_gpu, const int core_length,
+		const int core_dimen, const int *multi_nnz_train,
+		type_of_data **multi_value_train_device, int **multi_index_train_device,
+		type_of_data*** parameter_a_device, type_of_data** parameter_g_device,
+		type_of_data learn_rate_g, type_of_data lambda_g,
+		double* memcpy_time_g, const int model);
+
+void Mutil_GET_RMSE_AND_MAE(const int order, const int gpu_number,
+		const int multi_gpu_part, const int part_per_gpu, const int core_length,
+		const int core_dimen, const int *multi_nnz_train,
+		type_of_data **multi_value_train_device, int **multi_index_train_device,
+		type_of_data*** parameter_a_device, type_of_data** parameter_g_device,
+		type_of_data*** g_device, type_of_data* rmse, type_of_data* mae);
---
+++ Tucker/kernel.h
@@ -0,0 +1,30 @@
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+#include <cuda_fp16.h>
+
+#define type_of_data float
+
+void Update_Parameter_A(const int order, const int core_length,
+		const int core_dimen, type_of_data **parameter_a, const int nnz,
+		type_of_data **value, int **index, type_of_data **g,
+		const type_of_data learn_rate_a, const type_of_data lambda_a,
+		const int model);
+
+void Update_Parameter_G_Batch(const int order, const int core_length,
+		const int core_dimen, type_of_data **parameter_a,
+		type_of_data *parameter_g, const int nnz, type_of_data **value,
+		int **index, const type_of_data learn_rate_g,
+		const type_of_data lambda_g, const int model);
+
+void GET_RMSE_AND_MAE(const int order, const int core_length,
+		const int core_dimen, type_of_data **parameter_a,
+		type_of_data *parameter_g, const int nnz, type_of_data **value,
+		int **index, type_of_data **g, type_of_data *rmse,
+		type_of_data *mae);
+
+void GET_RMSE_AND_MAE(const int order, const int core_length,
+		const int core_dimen, type_of_data **parameter_a,
+		type_of_data *parameter_g, const int nnz, const type_of_data *value,
+		const int *index, type_of_data **g, type_of_data *rmse,
+		type_of_data *mae);
+
